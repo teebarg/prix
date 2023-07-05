82da9f891afda7040e5790993f52aa16559ae2fb
Adeniyi <teebarg01@gmail.com>
2023-07-05 13:56:22 +0100

    handle not found

---
+++ app/(store)/sub-category/[handle]/page.tsx
@@ -0,0 +1,4 @@
+export default async function ProductPage({ params }: { params: { handle: string } }) {
+  console.log('🚀 ~ file: page.tsx:2 ~ ProductPage ~ params:', params);
+  return <div className="px-12">This page is coming soon</div>;
+}
