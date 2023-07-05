7d705631a49aafeea97b97b3d3c1233ec9b117dd
Adeniyi <teebarg01@gmail.com>
2023-07-05 14:26:34 +0100

    update

---
+++ app/(store)/sub-category/[handle]/[id]/page.tsx
@@ -0,0 +1,4 @@
+export default async function SubSubCategory({ params }: { params: { id: string } }) {
+  console.log('🚀 ~ file: page.tsx:2 ~ ProductPage ~ params:', params);
+  return <div className="px-12">This page is coming soon</div>;
+}
